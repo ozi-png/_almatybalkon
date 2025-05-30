388a3f85d29a1e280d407b4898ec5d79691a5079
ozi-png <ozi.02.02.1996@mail.ru>
2025-05-30 22:16:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const slides = document.querySelector(".slides");
+    const slideImgs = document.querySelectorAll(".slides img");
+    const prev = document.querySelector(".prev");
+    const next = document.querySelector(".next");
+
+    let index = 0;
+
+    function showSlide(i) {
+      index = (i + slideImgs.length) % slideImgs.length;
+      slides.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    prev.addEventListener("click", () => showSlide(index - 1));
+    next.addEventListener("click", () => showSlide(index + 1));
+
+    showSlide(index);
+
+    // Автоматическое переключение каждые 5 секунд
+    setInterval(() => {
+      showSlide(index + 1);
+    }, 5000);
